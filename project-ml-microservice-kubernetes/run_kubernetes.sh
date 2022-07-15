03eb6a8069dc0de2a34e4be5ee6db72b77d79ab4
Bright Asechemie <brightasechemie@gmail.com>
2022-07-15 09:39:35 +0100

    updated configs

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+ dockerpath=tamunoseimiebi/ml-app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
 
+kubectl run ml-app  --image=$dockerpath
+
 
 # Step 3:
-# List kubernetes pods
+# List kubernetes  pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
 
+kubectl port-forward ml-app 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+ dockerpath=tamunoseimiebi/ml-app
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker login -u tamunoseimiebi
+docker tag ml-app $dockerpath:latest
 # Step 3:
 # Push image to a docker repository
+docker  push $dockerpath:latest
